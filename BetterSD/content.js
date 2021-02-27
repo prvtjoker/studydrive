35fc767f3f0af687c73ef6b96dca6929f82b842f
prvtjoker <79758700+prvtjoker@users.noreply.github.com>
2021-02-27 16:59:52 +0100

    Hello world

---
+++ BetterSD/content.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	/*let check = function(post) {
+	
+		let images = post.querySelectorAll(".img-container img");
+		images.forEach((image) => {
+			image.style.filter = 'blur(10px)';
+		});
+	
+	}
+
+	let observer = new MutationObserver((mutations) => {
+		mutations.forEach((mutation) => {
+			if (!mutation.addedNodes) return;
+			for (let i = 0; i < mutation.addedNodes.length; i++) {
+				let node = mutation.addedNodes[i];
+				if($(node).hasClass("mb-5")){
+					check(node);
+				}
+			}
+		});
+	});
+
+	observer.observe(document.body, {
+	    childList: true
+	  , subtree: true
+	  , attributes: false
+	  , characterData: false
+	});
+
+	var elements = $(".mb-5");
+
+	elements.each(function(post){
+		console.log("y");
+		check(post);
+	});*/
+
+});
